039300646a8c21d9d009fa5fedb29a286dabf787
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-08-12 11:57:45 +0300

    Build update: JS file compiled from server.ts

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,12 @@
+import { config } from 'dotenv';
 import { google } from 'googleapis';
 import nodemailer from 'nodemailer';
-const CLIENT_ID = '';
-const CLIENT_SECRET = '';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '';
+// load variables from .env into process.env
+config();
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 // allow this app to access a gmail address by using the CLIENT_ID, CLIENT_SECRET, REDIRECT_URI and REFRESH_TOKEN listed above
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
